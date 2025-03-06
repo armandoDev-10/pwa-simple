3e98d9ab167dce6403d4753fc46f54405169d8fa
armandoDev-10 <123055526+armandoDev-10@users.noreply.github.com>
2025-03-06 00:31:16 -0600

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,31 +1,19 @@
-const staticDevCoffee = "dev-coffee-site-v1"
-const assets = [
-  "/",
-  "/index.html",
-  "/css/style.css",
-  "/js/app.js",
-  "/images/coffee1.jpg",
-  "/images/coffee2.jpg",
-  "/images/coffee3.jpg",
-  "/images/coffee4.jpg",
-  "/images/coffee5.jpg",
-  "/images/coffee6.jpg",
-  "/images/coffee7.jpg",
-  "/images/coffee8.jpg",
-  "/images/coffee9.jpg",
-]
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('my-cache-name').then(cache => {
+      return cache.addAll([
+        '/',
+        '/css/style.css',
+        '/js/app.js'
+      ]);
+    }),
+  );
+});
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets)
-    })
-  )
-})
-self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-      caches.match(fetchEvent.request).then(res => {
-        return res || fetch(fetchEvent.request)
-      })
-    )
-  })
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    }),
+  );
+});
